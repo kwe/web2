902cc85735b804e865d38563be7e108140cd385b
kwe <kwevans@gmail.com>
2023-11-02 14:24:37 +0000

    Add fake crate for generating random names and add new route for outputing a random fake name

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,36 @@ use axum::routing::get;
 use axum::Router;
 use std::net::SocketAddr;
 use tower_http::services::ServeDir;
+use fake::faker::name::raw::*;
+use fake::locales::*;
+use fake::Fake;
+
 
 #[derive(Template)]
 #[template(path = "index.html")]
 struct HelloTemplate {
     name: String,
 }
+#[derive(Template)]
+#[template(path = "wow.html")]
+struct WowTemplate {
+    message: String,
+}
 
 async fn hello(Path(name): Path<String>) -> impl IntoResponse {
     HelloTemplate { name }
+}
+
+async fn wow() -> impl IntoResponse {
+    let message = Name(EN).fake();
+    WowTemplate { message}
 }
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/:name", get(hello))
+        .route("/wow", get(wow))
         .nest_service("/static", ServeDir::new("static/"));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
